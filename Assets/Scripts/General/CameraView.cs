339bff4f7600ffe631aafc91003f819972dbe067
“yuwen” <yuwen@kth.se>
2020-09-20 00:53:56 +0200

    add 3rd/1st/towards perspective cameras and put them in the same screen

---
+++ Assets/Scripts/General/CameraView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraView : MonoBehaviour
+{
+    public Transform player;
+
+    void LateUpdate(){
+        transform.position = player.position;
+        transform.rotation = player.rotation;
+    }
+    
+}
