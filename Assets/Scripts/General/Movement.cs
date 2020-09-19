f06bdb1c1a0407ae9422e44b514bae33aa1a9e23
“yuwen” <yuwen@kth.se>
2020-09-19 22:01:34 +0200

    attach the object to specific grid point when not clicking the object

--- Assets/Scripts/General/Movement.cs
+++ Assets/Scripts/General/Movement.cs
@@ -10,11 +10,18 @@ public class Movement : MonoBehaviour
     public Plane movePlane;
     public GridPointAttching grids;
 
-    // when the mouse is clicked on a collider
-    void OnMouseDown(){
-        movePlane = new Plane(Vector3.up, transform.position);
-        offset = mouseWorldPosition() - transform.position; 
-        
+    void checkNearstGridPoint(){
+        Vector2Int temp = getGirdIndex();
+        int i = temp.x;
+        int j = temp.y;
+        for (int m = 0; m < grids.gridNumber.y; m++){
+            for (int n = 0; n < grids.gridNumber.x; n++){
+                grids.gridVertices[m*grids.gridNumber.y+n].GetComponent<MeshRenderer>().material = grids.oriMat;
+            }
+        }
+        if (i >= 0 && i < grids.gridNumber.x && j >=  0 && j < grids.gridNumber.y){
+            grids.gridVertices[j*grids.gridNumber.y+i].GetComponent<MeshRenderer>().material = grids.nearMat;
+        }
     }
 
     Vector3 mouseWorldPosition(){
@@ -25,23 +32,31 @@ public class Movement : MonoBehaviour
         return rayToPlane.GetPoint(rayDis);
     }
 
+    // when the mouse is clicked on a collider
+    void OnMouseDown(){
+        movePlane = new Plane(Vector3.up, transform.position);
+        offset = mouseWorldPosition() - transform.position; 
+        
+    }
+
+    Vector2Int getGirdIndex(){
+        int i = Mathf.FloorToInt(((transform.position.x - (grids.leftBottomPosition.x - grids.gridSize/2f)) / grids.gridSize));
+        int j = Mathf.FloorToInt(((transform.position.z - (grids.leftBottomPosition.y - grids.gridSize/2f)) / grids.gridSize));
+        return new Vector2Int(i, j);
+    }
+
     // when the mouse is clicked on a collider and still holding it
     void OnMouseDrag(){
         transform.position = mouseWorldPosition() - offset;
         checkNearstGridPoint();
     }
 
-    void checkNearstGridPoint(){
-        int i = Mathf.FloorToInt(((transform.position.x - (grids.leftBottomPosition.x - grids.gridSize/2f)) / grids.gridSize));
-        int j = Mathf.FloorToInt(((transform.position.z - (grids.leftBottomPosition.y - grids.gridSize/2f)) / grids.gridSize));
-        for (int m = 0; m < grids.gridNumber.y; m++){
-            for (int n = 0; n < grids.gridNumber.x; n++){
-                grids.gridVertices[m*grids.gridNumber.y+n].GetComponent<MeshRenderer>().material = grids.oriMat;
-            }
-        }
-        if (i >= 0 && i < grids.gridNumber.x && j >=  0 && j < grids.gridNumber.y){
-            grids.gridVertices[j*grids.gridNumber.y+i].GetComponent<MeshRenderer>().material = grids.nearMat;
-        }
+    //when the mouse exit the collider
+    void OnMouseUpAsButton(){
+        Vector2Int temp = getGirdIndex();
+        transform.position = new Vector3(grids.leftBottomPosition.x + temp.x*grids.gridSize, 
+                                        transform.position.y,
+                                        grids.leftBottomPosition.y + temp.y*grids.gridSize);
     }
 
     // Start is called before the first frame update
