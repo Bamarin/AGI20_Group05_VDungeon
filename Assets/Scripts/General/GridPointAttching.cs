3362a0e45dd462bed7718852eab5222f9f4fe543
“yuwen” <yuwen@kth.se>
2020-09-19 20:29:01 +0200

    enable dragging on the plane

---
+++ Assets/Scripts/General/GridPointAttching.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPointAttching : MonoBehaviour
+{
+    [Header("Grid Parameters")]
+    [Range(1f, 10f)]
+    public float gridSize = 1f; 
+    public Vector2Int gridNumber = new Vector2Int(10, 10);
+    public Vector2 leftBottomPosition = new Vector2(-5f, -5f);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //create grid points
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/General/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //keep the offset from the the object center to the mouse clicked position
+    public Vector3 offset;
+    //the plane where the object is moving on
+    public Plane movePlane;
+
+    // when the mouse is clicked on a collider
+    void OnMouseDown(){
+        movePlane = new Plane(Vector3.up, transform.position);
+        offset = mouseWorldPosition() - transform.position; 
+        
+    }
+
+    Vector3 mouseWorldPosition(){
+        //cast a ray along the camera to the plane
+        Ray rayToPlane = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float rayDis;
+        movePlane.Raycast(rayToPlane, out rayDis);
+        return rayToPlane.GetPoint(rayDis);
+    }
+    // when the mouse is clicked on a collider and still holding it
+    void OnMouseDrag(){
+        transform.position = mouseWorldPosition() - offset;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
