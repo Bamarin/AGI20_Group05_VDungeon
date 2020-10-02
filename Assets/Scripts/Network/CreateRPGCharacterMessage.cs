68429ff8984bdd9c91fd5bcd856824533b543666
bamarin <mauithebest@libero.it>
2020-10-02 09:59:31 +0200

    use characters prfabs

---
+++ Assets/Scripts/Network/CreateRPGCharacterMessage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CreateRPGCharacterMessage : MessageBase
+{
+    public Race race;
+    public string name;
+    public Color skinColor;
+    public Class characterClass;
+}
+
+public enum Race
+{
+    None,
+    Elvish,
+    Dwarvish,
+    Human
+}
+
+public enum Class
+{
+    Barbarian,
+    Bard,
+    Cleric,
+    Druid,
+    Fighter,
+    Monk,
+    Paladin,
+    Ranger,
+    Rogue,
+    Sorcerer,
+    Warlock,
+    Wizard
+}
