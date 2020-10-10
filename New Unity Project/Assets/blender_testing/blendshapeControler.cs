6b38f2e823782b00022b21ad7be69b3cf97598b5
Mjaomjao2 <71384569+Mjaomjao2@users.noreply.github.com>
2020-10-10 15:25:46 +0200

    just testing stuff

---
+++ New Unity Project/Assets/blender_testing/blendshapeControler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blendshapeControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SkinnedMeshRenderer cube;
+    void Start()
+    {
+        cube = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cur = cube.GetBlendShapeWeight(0) + 1;
+        cur = cur % 101;
+        cube.SetBlendShapeWeight(0, cur);
+        cube.SetBlendShapeWeight(1, 100 - cur);
+        
+    }
+}
