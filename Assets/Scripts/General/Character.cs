514804cfd5b0ef80879c56a0ea6a271c60b25ea7
bamarin <mauithebest@libero.it>
2020-11-08 09:25:34 +0100

    incorporate mirror

--- Assets/Scripts/General/Character.cs
+++ Assets/Scripts/General/Character.cs
@@ -91,9 +91,4 @@ public class Character : Entity
         }
     }
 
-    void LateUpdate()
-    {
-        faceCam.transform.position = this.transform.position + faceCam.GetComponent<CameraTowards>().offset;
-        faceCam.transform.LookAt(this.transform);
-    }
 }
